e8257636e33be01e5afc4a200748c15c51102e90
Facebookuser043 <facebookuser043@gmail.com>
2024-05-12 01:13:14 +0200

    Update noti.py

--- noti.py
+++ noti.py
@@ -23,11 +23,4 @@ project_home = '/home/facebookuser043/mysite'
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-# set environment variable to tell django where your settings.py is
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-
-
-# serve django via WSGI
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
 
