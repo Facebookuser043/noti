3c882f97dcf915536eea633ba75184273ab52763
Facebookuser043 <facebookuser043@gmail.com>
2024-05-12 00:19:06 +0200

    Update noti.py

--- noti.py
+++ noti.py
@@ -1,20 +1,13 @@
 import requests
 
-# Open the file in write mode
-file = open("output.html", "w")
-
-# Write HTML content
-file.write("<html>")
-file.write("<head>")
-file.write("<title>My Webpage</title>")
-file.write("</head>")
-file.write("<body>")
-file.write("<h1>Welcome to my webpage!</h1>")
-file.write("</body>")
-file.write("</html>")
-
-# Close the file
-file.close()
+import cgi 
+print "Content-type: text/html" 
+print """<html> 
+<head><title>My first Python CGI app</title></head> 
+<body> 
+<p>Hello, 'world'!</p> 
+</body> 
+</html>""" 
 
 print("HTML file successfully written.")
 
