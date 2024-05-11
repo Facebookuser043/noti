0a58312981c1ec79c44ea67c980c981372baef9e
Facebookuser043 <facebookuser043@gmail.com>
2024-05-11 23:53:28 +0200

    Update noti.py

--- noti.py
+++ noti.py
@@ -2,7 +2,7 @@ import requests
 
 response = requests.post("https://studynotification.app.n8n.cloud/webhook/64e9e6b2-0695-415a-a82e-9e498942915f", json={
     "userId": 1,
-    "title": "Online Script is Working",
+    "title": "New Try",
     "completed": False
 }, headers={
     "Content-type": "application/json; charset=UTF-8"
