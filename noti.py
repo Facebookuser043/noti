fbfb6361acccf01cb74e2158afb6eee18cf04604
Facebookuser043 <facebookuser043@gmail.com>
2024-05-12 00:11:26 +0200

    Update noti.py

--- noti.py
+++ noti.py
@@ -1,5 +1,6 @@
 import requests
 
+print("h")
 response = requests.post("https://studynotification.app.n8n.cloud/webhook/64e9e6b2-0695-415a-a82e-9e498942915f", json={
     "userId": 1,
     "title": "New Try",
