e6fcdd944d53f0cf1d86ebe3574b496adbfa2936
Facebookuser043 <facebookuser043@gmail.com>
2024-05-12 00:22:33 +0200

    Update noti.py

--- noti.py
+++ noti.py
@@ -1,21 +1,6 @@
 import requests
 
-import commands 
- 
-print "content-type: text/html" 
-print 
- 
-#Now you can write html code in print statement 
-print """ 
-<table> 
-<form action=""> 
-<input type="pass" name="pass"> 
-<input type="Submit"> 
-</form> 
-</table>  
-""" 
 
-print("HTML file successfully written.")
 
 response = requests.post("https://studynotification.app.n8n.cloud/webhook/64e9e6b2-0695-415a-a82e-9e498942915f", json={
     "userId": 1,
