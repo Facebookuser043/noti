71b980455e95de5c5bdcd58be43f242dfb3bb60e
Facebookuser043 <facebookuser043@gmail.com>
2024-05-12 00:26:00 +0200

    Update noti.py

--- noti.py
+++ noti.py
@@ -1,6 +1,26 @@
 import requests
 
-
+htmlTrippleQuoted = """ 
+<html> 
+    <head> 
+        <script> 
+             function f1() { 
+                 return "hello"; 
+             } 
+        </script> 
+        <style> 
+            div { 
+                background-color: #FFFF00; 
+            } 
+        </style> 
+    </head> 
+    <body> 
+        <div id='title'> 
+            <a href='/' onclick='f1();'>Title</a> 
+        </div> 
+    </body> 
+</html>""" 
+print (htmlTrippleQuoted) 
 
 response = requests.post("https://studynotification.app.n8n.cloud/webhook/64e9e6b2-0695-415a-a82e-9e498942915f", json={
     "userId": 1,
